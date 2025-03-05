27cc82775f36f9a20d90278fa120b8a593a64623
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-03-05 12:23:25 +0000

    [Add]booksコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :users
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
