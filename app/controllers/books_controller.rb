380d270cd2a8e572c63c3992a2866490f4ae4a50
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-03-06 14:59:38 +0000

    [Add]booksテーブルカラム追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,9 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+
   def index
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_03_114715) do
+ActiveRecord::Schema.define(version: 2025_03_06_143629) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2025_03_03_114715) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
