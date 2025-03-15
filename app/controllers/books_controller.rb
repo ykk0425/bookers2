f25128236869f7810412ab37897031bcdac492e5
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-03-15 08:25:12 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -26,6 +27,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] ="You have updated book successfully."
     redirect_to book_path(book)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,27 @@ class UsersController < ApplicationController
 
   def index
     @user = current_user
-    @new_book = Book.new
+    @book = Book.new
     @users = User.all  
   end
 
   def show
-    @new_book = Book.new
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    flash[:notice] ="You have updated user successfully."
     redirect_to user_path(@user)
   end
   
