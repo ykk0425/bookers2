3f971d35ebf3affcccd69030a1951ce20ab9d407
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-03-15 10:11:49 +0000

    [Add]バリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
-  has_one_attached :profile_image
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :profile_image
+  
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
